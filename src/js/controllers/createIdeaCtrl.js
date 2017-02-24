5ffe19238aa7ffaa851d9e9989966845b38204a2
Arturo Mendoza <hello@arturomendoza.me>
2017-02-24 13:11:09 -0600

    CreateIdea Tags are working without design

--- src/js/controllers/createIdeaCtrl.js
+++ src/js/controllers/createIdeaCtrl.js
@@ -5,20 +5,54 @@ angular.module('musementApp')
       $scope.idea = {};
       $scope.mySwitch = false;
       $scope.inputTeamMembers = false;
+      $scope.tags = []
 
-      $scope.submitIdea = function () {
+
+      // Load categories when creating a moment
+      $scope.loadTags = function($query) {
+        return $http.get(HOST + '/api/tags',{cache: true}).then(function(response) {
+          var tags = response.data;
+          return tags.filter(function(tag) {
+            return tag.name.toLowerCase().indexOf($query.toLowerCase()) != -1;
+          })
+        })
+      }
+
+      $scope.createIdea = function() {
+        if (this.idea.banner == undefined) {
+           $scope.sumbitIdea("/static/img/cover_Login.svg")
+        }else if (this.idea.banner.type) {
+          $scope.upload(this.idea.banner)
+        }
+
+      }
+
+      $scope.upload = function(file){
+        if (!file) { //If user doesnt want to upload a photo, set the gravatar one
+            $scope.submitIdea(); //Send no image
+        } else {
+          Upload.upload({url: window.HOST + '/api/upload', data:{ file: file }})
+          .then(function (res) { //upload function returns a promise
+                $scope.submitIdea('/static/uploads/' + res.data.file_name);
+            }, function (errRes) { //catch error
+                $window.alert('Error status: ' + errRes.status);
+          });
+        }
+      }
+
+      $scope.submitIdea = function (banner) {
         console.log("sumbitIdea Execute");
         let ideaInfo = {};
         ideaInfo.name = this.idea.name
         ideaInfo.description = this.idea.description
         ideaInfo.categories = this.idea.categories
         ideaInfo.problem = this.idea.problem
-        // ideaInfo.cover = cover
+        ideaInfo.banner = banner
 
         createIdeaDataService.setIdea(ideaInfo, user_id, function(res){
           if (res.status == 201) {
             // $scope.this_user.ideas.push(res.data.idea)
-            $state.go('home')
+            $state.go('showIdea')
           }
         });
       }
