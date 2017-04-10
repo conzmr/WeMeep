a687f10020a79799774935f201c774ebb561eaff
Constanza Madrigal Reyes <cmr_1821@hotmail.com>
2017-04-10 15:46:27 -0500

    MyIdea redesigned.

--- src/js/controllers/myIdeaCtrl.js
+++ src/js/controllers/myIdeaCtrl.js
@@ -1,11 +1,34 @@
 angular.module('musementApp')
     .controller('myIdeaCtrl', function($scope) {
-        $scope.pivotear = false;
-        //pivotear=false when is clicked outside the modal window??
+        $scope.pivoting = false;
+        $scope.notification = false;
+        $scope.showNotifications=false;
+        $scope.showUserMenu=false;
+        $scope.showPivots = false;
+        $scope.pivots = ['1th Pivot', '2nd Pivot', '3th Pivot'];
+        $scope.pivotSelected = $scope.pivots[0];
+
+        $scope.createPivot = function(){
+          $scope.pivoting = true;
+          $scope.showPivots =false;
+        }
+
+
+        $scope.changeShowNotifications = function(){
+          $scope.showNotifications = !$scope.showNotifications;
+        }
+
+        $scope.changeShowMenu = function(){
+          $scope.showUserMenu = !$scope.showUserMenu;
+        }
+
+        $scope.selectPivot = function(pivot){
+          $scope.pivotSelected = pivot;
+          $scope.showPivots = false;
+        }
+
+        $scope.changeShowPivots = function(){
+          $scope.showPivots = !$scope.showPivots;
+        }
+
     })
-    // var pivModal = document.getElementById('Pivotear-modal');
-    // window.onclick = function(event) {
-    //     if (event.target == pivModal || event.target == closePivModal || event.target == newPivBtn) {
-    //         pivModal.style.display = "none";
-    //     }
-    // }
