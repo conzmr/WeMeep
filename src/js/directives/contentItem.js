4687f6662a343a67488201fe9e413eb77e9baf40
Constanza Madrigal Reyes <cmr_1821@hotmail.com>
2017-05-10 13:49:19 -0500

    Fixed error in contentItem.

--- src/js/directives/contentItem.js
+++ src/js/directives/contentItem.js
@@ -1,32 +1,32 @@
 angular.module('musementApp')
-.directive('contentItem', function ($compile, TemplateService) {
-    var getTemplate = function (contentType) {
-        var template = '';
-        switch (contentType) {
-            case 'scale':
-                template = '/static/views/templates/testScale.html',
-            case 'check':
-                template = '/static/views/templates/testCheck.html';
-        }
-        return template;
-    };
+    .directive('contentItem', function($compile, TemplateService) {
+        var getTemplate = function(contentType) {
+            var template = '';
+            switch (contentType) {
+                case 'scale':
+                    template = '/static/views/templates/testScale.html';
+                case 'check':
+                    template = '/static/views/templates/testCheck.html';
+            }
+            return template;
+        };
 
-    var linker = function (scope, element, attrs) {
-        scope.rootDirectory = 'images/';
+        var linker = function(scope, element, attrs) {
+            scope.rootDirectory = 'images/';
 
-        TemplateService.getTemplates().then(function (response) {
+            TemplateService.getTemplates().then(function(response) {
 
-            element.html(getTemplate(scope.content.content_type));
+                element.html(getTemplate(scope.content.content_type));
 
-            $compile(element.contents())(scope);
-        });
-    };
+                $compile(element.contents())(scope);
+            });
+        };
 
-    return {
-        restrict: 'E',
-        link: linker,
-        scope: {
-            content: '='
-        }
-    };
-});
+        return {
+            restrict: 'E',
+            link: linker,
+            scope: {
+                content: '='
+            }
+        };
+    });
